0320f802993c94168aedf71eab8c49dedc20202b
DayanaOtagri <heidyotagri@hotmail.com>
2023-06-30 02:16:58 -0500

    No funciona traer comic por id y no cambia de pagina

--- pages/Comic/[id].tsx
+++ pages/Comic/[id].tsx
@@ -7,40 +7,73 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await getComics();
+  try {
+    const response = await getComics();
 
-  const paths = response.data.results.map(({ id }: { id: any }) => ({
-    params: {
-      id: id?.toString()
-    }
-  }));
+    const paths = response?.data?.results?.map(({ id }: { id: number }) => ({
+      params: {
+        id: id?.toString()
+      }
+    }));
 
-  return {
-    paths,
-    fallback: 'blocking' 
-  };
+    return {
+      paths: paths || [],
+      fallback: 'blocking'
+    };
+  } catch (error) {
+    console.error('Error retrieving comics for paths:', error);
+    return {
+      paths: [],
+      fallback: 'blocking'
+    };
+  }
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const id = parseInt(params?.id as string);
-  const comic = await getComic(id);
-  const characters = await getCharacterByComic(id);
+  try {
+    const id = parseInt(params?.id as string, 10);
+    const comic = await getComic(id);
+    const characters = await getCharacterByComic(id);
 
-  return {
-    props: {
-      comic,
-      characters
-    },
-    revalidate: 10,
-  };
+    return {
+      props: {
+        comic,
+        characters
+      },
+      revalidate: 10,
+    };
+  } catch (error) {
+    console.error('Error retrieving comic and characters:', error);
+    return {
+      notFound: true
+    };
+  }
 };
 
 function ComicDetails({ comic, characters }: { comic: any, characters: any }) {
+  if (!comic) {
+    return (
+      <>
+        <Head>
+          <title>Error | DH MARVEL</title>
+          <meta name="description" content="Error al cargar el cómic" />
+        </Head>
+        <LayoutGeneral>
+          <Box sx={{ marginBottom: '1rem' }}>
+            <BodySingle title='Error al cargar el cómic'>
+              <p>Hubo un error al cargar el cómic. Por favor, inténtalo nuevamente más tarde.</p>
+            </BodySingle>
+          </Box>
+        </LayoutGeneral>
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
         <title>{comic?.title} | DH MARVEL</title>
-        <meta name="description" content={`${comic?.title}: página detalle de cómic `} />
+        <meta name="description" content={`${comic?.title}: página detalle de cómic`} />
       </Head>
       <LayoutGeneral>
         <Box sx={{ marginBottom: '1rem' }}>
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 import BodySingle from "dh-marvel/components/layouts/body/single/body-single";
 import { getComics } from 'dh-marvel/services/marvel/marvel.service';
 import { useEffect, useState } from 'react';
@@ -7,65 +7,69 @@ import ResponsiveGrid from 'dh-marvel/components/Grid/Grid';
 import PaginationOutlined from 'dh-marvel/components/Pagination/Pagination';
 import LayoutGeneral from 'dh-marvel/components/layouts/layout-general';
 
-const INITIAL_OFFSET = 0
-const INITIAL_LIMIT = 12
+const INITIAL_OFFSET = 0;
+const INITIAL_LIMIT = 12;
 
 export async function getServerSideProps() {
-    const response = await getComics(INITIAL_OFFSET, INITIAL_LIMIT);
-    const initialComics = response?.data?.results || [];
-    const limit = response?.data?.count ?? null;
-    const initialTotal = response?.data?.total ?? null;
-    
-    return {
-      props: {
-        initialComics,
-        limit,
-        initialTotal
-      }
-    };
-  }
+  const response = await getComics(INITIAL_OFFSET, INITIAL_LIMIT);
+  const initialComics = response?.data?.results || [];
+  const limit = response?.data?.count ?? null;
+  const initialTotal = response?.data?.total ?? null;
+
+  return {
+    props: {
+      initialComics,
+      limit,
+      initialTotal
+    }
+  };
+}
 
 type indexProps = {
-    initialComics: any;
-    initialTotal: number,
+  initialComics: any;
+  initialTotal: number;
 }
 
 const Index: NextPage<indexProps> = ({ initialComics, initialTotal }) => {
-    const [comics, setComics] = useState(initialComics)
-    const [page, setPage] = useState(1);
-    const [total, settotal] = useState(initialTotal);
-    const LIMIT = 12
+  const [comics, setComics] = useState(initialComics);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(initialTotal);
+  const LIMIT = 12;
 
-    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        setPage(value);
-    };
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
+  useEffect(() => {
+    let offset = LIMIT * (page - 1);
+    getComics(offset, LIMIT).then(response => {
+      setComics(response?.data?.results);
+      setTotal(response?.data?.total);
+    });
 
-    useEffect(() => {
-        let offset = LIMIT * (page - 1)
-        getComics(offset, LIMIT).then(response => {
-            setComics(response?.data?.results)
-            settotal(response?.data?.total)
-        })
+    localStorage.clear();
+  }, [page]);
 
-        localStorage.clear();
-    }, [page])
-
-    return (
-        <>
-            <Head>
-                <title>Inicio | DH MARVEL</title>
-                <meta name="description" content="Marvel Store Sitio Web" />
-            </Head>
-            <LayoutGeneral>
-                <BodySingle title={"¡Hola disfruta Marvel Store!"}>
-                    <PaginationOutlined count={Math.round(total / 12)} page={page} handleChange={handleChange} />
-                    <ResponsiveGrid data={comics} />
-                    <PaginationOutlined count={Math.round(total / 12)} page={page} handleChange={handleChange} />
-                </BodySingle>
-            </LayoutGeneral>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Inicio | DH MARVEL</title>
+        <meta name="description" content="Marvel Store Sitio Web" />
+      </Head>
+      <LayoutGeneral>
+        <BodySingle title={"¡Hola disfruta Marvel Store!"}>
+          <PaginationOutlined count={Math.ceil(total / LIMIT)} page={page} handleChange={handleChange} />
+          <ResponsiveGrid data={comics} />
+          <PaginationOutlined count={Math.ceil(total / LIMIT)} page={page} handleChange={handleChange} />
+        </BodySingle>
+      </LayoutGeneral>
+    </>
+  )
 }
 
-export default Index
+export default Index;
+
+
+console.log(process.env.MARVEL_API_URL);
+console.log(process.env.MARVEL_API_PUBLIC_KEY);
+console.log(process.env.MARVEL_API_PRIVATE_KEY);
